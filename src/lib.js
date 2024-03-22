a2a4c9a27564698ee67adc1b9cacdf591118397b
Jason <jasongitmail@gmail.com>
2024-03-22 23:22:16 +0000

    Link to actions tab instead of cron.yml itself

--- src/lib.js
+++ src/lib.js
@@ -103,7 +103,7 @@ Compares UI components available in:
 - [bits-ui](https://www.bits-ui.com/docs/components)
 - [melt-ui](https://melt-ui.com/docs/builders/accordion)
 
-Last updated: ${currentDate}. Updates checked daily via [cron.yml](.github/workflows/cron.yml).
+Last updated: ${currentDate}. Updates checked daily via [cron.yml](https://github.com/jasongitmail/shadcn-compare/actions/workflows/cron.yml).
 
 Jump to table:
 - [Shadcn](#shadcn)
